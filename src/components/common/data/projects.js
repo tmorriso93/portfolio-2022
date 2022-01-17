8776d52062612a86d980744a89269aaa720bd963
T M <tmorriso93@gmail.com>
2022-01-17 18:36:59 -0500

    add real estate project

--- src/components/common/data/projects.js
+++ src/components/common/data/projects.js
@@ -1,6 +1,16 @@
 export const ProjectData = [
   {
     id: 1,
+    title: "Real Estate React Application",
+    about:
+      "United Arab Emerits real estate app made with ReactJs and Nextjs using the Bayut API.",
+    tags: ["react", "nextJs"],
+    demo: "https://uae-real-estate-app.netlify.app/",
+    github: "https://github.com/tmorriso93/react-real-estate-app",
+    image: require("../../../assets/tmorriso-uae-realestate.png"),
+  },
+  {
+    id: 2,
     title: "HTML Email #1",
     about: "repsonsive email marketing template",
     tags: ["html", "inline css", "tables"],
@@ -8,12 +18,9 @@ export const ProjectData = [
     github:
       "https://github.com/tmorriso93/email-template-01/blob/main/index.html",
     image: require("../../../assets/tmorriso-email01.png"),
-
-    // image:
-    //   "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fblog.hubspot.com%2Fhs-fs%2Fhubfs%2Fthemezy.jpg%3Ft%3D1512171168621%26width%3D640%26name%3Dthemezy.jpg&f=1&nofb=1",
   },
   {
-    id: 2,
+    id: 3,
     title: "HTML Email #2",
     about: "indeed.com HTML email clone for job seekers",
     tags: ["html", "inline css", "tables"],
@@ -21,17 +28,17 @@ export const ProjectData = [
     github: "https://github.com/tmorriso93/email-template-02",
     image: require("../../../assets/tmorriso-email02.png"),
   },
+  // {
+  //   id: 4,
+  //   title: "HTML #3",
+  //   about: "repsonsive email template made to fit on all devices",
+  //   tags: ["html", "inline css", "tables"],
+  //   demo: "https://tmorriso93.github.io/email-template-03/",
+  //   github: "https://github.com/tmorriso93/email-template-03",
+  //   image: require("../../../assets/tmorriso-email03.png"),
+  // },
   {
-    id: 3,
-    title: "HTML #3",
-    about: "repsonsive email template made to fit on all devices",
-    tags: ["html", "inline css", "tables"],
-    demo: "https://tmorriso93.github.io/email-template-03/",
-    github: "https://github.com/tmorriso93/email-template-03",
-    image: require("../../../assets/tmorriso-email03.png"),
-  },
-  {
-    id: 4,
+    id: 5,
     title: "Netflix",
     about:
       "Netflix, Inc. is an American subscription streaming service and production company. | 2.50 Billion monthly visits ",
@@ -41,7 +48,7 @@ export const ProjectData = [
     image: require("../../../assets/tmorriso-netflixpage.png"),
   },
   {
-    id: 5,
+    id: 6,
     title: "Hulu",
     about:
       "Hulu is an American streaming platform. It offers a library of films and television series. | 215 Million monthly visits ",
@@ -51,7 +58,7 @@ export const ProjectData = [
     image: require("../../../assets/tmorriso-hulupage.png"),
   },
   {
-    id: 6,
+    id: 7,
     title: "Starbucks",
     about:
       "Starbucks Corporation is an American multinational chain of coffeehouses. It's the world's largest coffeehouse chain. | 23 Million monthly visits",
@@ -61,7 +68,7 @@ export const ProjectData = [
     image: require("../../../assets/tmorriso-starbuckspage.png"),
   },
   {
-    id: 7,
+    id: 8,
     title: "Microsoftpage",
     about:
       "Microsoft Corporation produces computer software, consumer electronics, personal computers, and related services. | 1 Billion monthly visits",
